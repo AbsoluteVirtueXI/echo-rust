3dc61d17b53fd400f72a3f5ba3be4e4cc7147c9b
Absolute Virtue <AbsoluteVirtueXI@gmail.com>
2020-05-06 08:17:10 +0200

    using stream for looping over incoming connection

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,16 +1,35 @@
 use tokio::io;
 use tokio::net::{TcpListener, TcpStream};
+use tokio::stream::{StreamExt};
 
+use std::process::exit;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut listener = TcpListener::bind("127.0.0.1:7777").await?;
-    loop {
-        let (stream, socket) = listener.accept().await?;
-        println!("Connection from {}:{}", socket.ip(), socket.port());
-        echo(stream).await?;
-        println!("Disconnection from {}:{}", socket.ip(), socket.port());
+    let mut listener = match TcpListener::bind("127.0.0.1:7777").await {
+        Ok(listener) => listener,
+        Err(e) => {
+            println!("Server can't bind on ip:port. Error: {}", e);
+            exit(-1);
+        }
+    };
+
+    while let Some(tcp_stream) = listener.incoming().next().await {
+        match tcp_stream {
+            Ok(connection) => {
+                let peer_addr = connection.peer_addr()?;
+                println!("Connection from {}:{}", peer_addr.ip(), peer_addr.port());
+                echo(connection).await?;
+                println!("Disconnection from {}:{}", peer_addr.ip(), peer_addr.port());
+            }
+            Err(e) => {
+                println!("Connection Error: {}", e);
+            }
+        }
     }
+
+    Ok(())
+
 }
 
 async fn echo(stream: TcpStream) -> io::Result<()> {
