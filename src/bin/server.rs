9b758fef3fa7547fb0cff3369e85c67886c3158a
Absolute Virtue <AbsoluteVirtueXI@gmail.com>
2020-05-06 04:42:04 +0200

    first naive version of an echo server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,20 @@
-use tokio::task::spawn_blocking;
+use tokio::io;
+use tokio::net::{TcpListener, TcpStream};
+
 
 #[tokio::main]
-async fn main() {
-    spawn_blocking(|| {
-        println!("Hello from server.");
-    });
+async fn main() -> io::Result<()> {
+    let mut listener = TcpListener::bind("127.0.0.1:7777").await?;
+    loop {
+        let (stream, socket) = listener.accept().await?;
+        println!("Connection from {}:{}", socket.ip(), socket.port());
+        echo(stream).await?;
+        println!("Disconnection from {}:{}", socket.ip(), socket.port());
+    }
+}
+
+async fn echo(stream: TcpStream) -> io::Result<()> {
+    let (mut recv, mut send) = io::split(stream);
+    io::copy(&mut recv, &mut send).await?;
+    Ok(())
 }
