879a7ee1b1f67d24613b91e89fb715a3acc35ea1
Absolute Virtue <AbsoluteVirtueXI@gmail.com>
2020-05-07 10:13:59 +0200

    Naive implementaton of an echo client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,8 +1,20 @@
-use tokio::task::spawn_blocking;
+use tokio::net::TcpStream;
+use tokio::io;
 
 #[tokio::main]
-async fn main() {
-    spawn_blocking(|| {
-        println!("Hello from client.");
+async fn main() -> io::Result<()>  {
+    let mut stream = TcpStream::connect("127.0.0.1:7777").await?;
+    let (mut recv, mut send) = io::split(stream);
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+    let sendx = tokio::spawn ( async move {
+        io::copy(&mut stdin, &mut send).await
     });
+    let recvx = tokio::spawn( async move {
+        io::copy(&mut recv, &mut stdout).await
+    });
+    sendx.await?;
+    //recvx.await?;
+    println!("END OF PROGRAM CLIENT.");
+    Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -6,34 +6,37 @@ use std::process::exit;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    /// Bind listener to a port
     let mut listener = match TcpListener::bind("127.0.0.1:7777").await {
-        Ok(listener) => listener,
+        Ok(listener) => {
+            println!("Server running");
+            listener
+        },
         Err(e) => {
             println!("Server can't bind on ip:port. Error: {}", e);
             exit(-1);
         }
     };
 
+    /// Loop over incoming connection
     while let Some(tcp_stream) = listener.incoming().next().await {
         match tcp_stream {
             Ok(connection) => {
-                let peer_addr = connection.peer_addr()?;
-                println!("Connection from {}:{}", peer_addr.ip(), peer_addr.port());
-                echo(connection).await?;
-                println!("Disconnection from {}:{}", peer_addr.ip(), peer_addr.port());
+                tokio::spawn(echo(connection));
             }
             Err(e) => {
                 println!("Connection Error: {}", e);
             }
         }
     }
-
     Ok(())
-
 }
 
 async fn echo(stream: TcpStream) -> io::Result<()> {
+    let peer_addr = stream.peer_addr()?;
+    println!("Connection from {}:{}", peer_addr.ip(), peer_addr.port());
     let (mut recv, mut send) = io::split(stream);
     io::copy(&mut recv, &mut send).await?;
+    println!("Disconnection from {}:{}", peer_addr.ip(), peer_addr.port());
     Ok(())
 }
