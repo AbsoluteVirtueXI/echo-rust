caeee00f366e5f4b9936b49fb2d2bd16cdcc00a6
Absolute Virtue <AbsoluteVirtueXI@gmail.com>
2020-05-09 03:06:10 +0200

    added TcpServer struct in easy_net module

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -10,10 +10,10 @@ use echo_rust::easy_net::*;
 #[tokio::main]
 async fn main() -> io::Result<()> {
     // Bind listener to a port
-    let mut listener = match TcpListener::bind("127.0.0.1:7777").await {
-        Ok(listener) => {
+    let mut server = match TcpServer::bind("127.0.0.1:7777").await {
+        Ok(server) => {
             println!("Server running");
-            listener
+            server
         },
         Err(e) => {
             println!("Server can't bind on ip:port. Error: {}", e);
@@ -23,24 +23,12 @@ async fn main() -> io::Result<()> {
 
 
     // Loop over incoming connection
-    while let Some(tcp_stream) = listener.incoming().map(|res_stream|{
-        match res_stream {
-            Ok(stream) => Ok(TcpConnection::new(stream)),
-            Err(e) => Err(e)
-        }
-    }).next().await {
-        match tcp_stream {
-            Ok(connection) => {
-                tokio::spawn(echo(connection));
-            }
-            Err(e) => {
-                println!("Connection Error: {}", e);
-            }
-        }
-    }
+    server.run().await;
+
     Ok(())
 }
 
+/*
 /// Handle the connection of the echo client
 async fn echo(stream: TcpConnection) -> io::Result<()> {
     let stream = stream.stream;
@@ -50,4 +38,4 @@ async fn echo(stream: TcpConnection) -> io::Result<()> {
     io::copy(&mut recv, &mut send).await?;
     println!("Disconnection from {}:{}", peer_addr.ip(), peer_addr.port());
     Ok(())
-}
+}*/
--- src/easy_net.rs
+++ src/easy_net.rs
@@ -4,7 +4,8 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::time::SystemTime;
 use std::mem::size_of_val;
 use tokio::net::{TcpListener, TcpStream};
-
+use tokio::io;
+use tokio::stream::{StreamExt};
 
 const UNSPECIFIED_IP: Ipv4Addr = Ipv4Addr::UNSPECIFIED;
 const UNSPECIFIED_PORT: u16 = 0;
@@ -12,44 +13,104 @@ const UNSPECIFIED_PORT: u16 = 0;
 fn _get_local_SocketAddr(stream: &TcpStream) -> SocketAddr {
     match stream.local_addr() {
         Ok(socket) => socket,
-        Err(e) => _get_unspecified_SocketAddr()
+        Err(_) => _get_unspecified_SocketAddr()
     }
 }
 
 fn _get_peer_SocketAddr(stream: &TcpStream) -> SocketAddr {
     match stream.peer_addr() {
         Ok(socket) => socket,
-        Err(e) => _get_unspecified_SocketAddr()
+        Err(_) => _get_unspecified_SocketAddr()
     }
 }
 
 fn _get_unspecified_SocketAddr() -> SocketAddr {
     SocketAddr::V4(SocketAddrV4::new(UNSPECIFIED_IP, UNSPECIFIED_PORT))
 }
-
 /*
+enum TransportLayer {
+    Tcp,
+    Udp,
+}
+
+pub enum Server {
+    TcpServer(TcpServer),
+    UdpServer(UdpServer),
+}
+
+impl Server {
+    pub async fn new(transport: TransportLayer) -> io::Result<Server> {
+        match transport {
+            TransportLayer::Tcp => Server::TcpServer(TcpServer::new().await),
+            TransportLayer::Udp => unimplemented!(),
+        }
+    }
+}
+*/
+enum Client {
+    TcpClient(TcpClient),
+    UdpClient(UdpClient),
+}
+
+// TODO: add a connection pool to avoid DOS
 pub struct TcpServer {
-    server : TcpListener,
+    pub listener: TcpListener,
 }
 
 impl TcpServer {
-    pub async fn bind() {}
-    pub async fn start() {}
+    pub async fn bind(s: &str) -> io::Result<TcpServer> {
+        let res_server = TcpListener::bind("127.0.0.1:7777").await;
+        match res_server {
+            Ok(listener) => Ok(TcpServer { listener }),
+            Err(e) => Err(e)
+        }
+    }
+    pub async fn run(&mut self) {
+        while let Some(tcp_stream) = self.listener.incoming().map(|res_stream| {
+            match res_stream {
+                Ok(stream) => Ok(TcpConnection::new(stream)),
+                Err(e) => Err(e), // TODO: maybe i can handle error here directly
+            }
+        }).next().await {
+            match tcp_stream {
+                Ok(connection) => {
+                    println!("Connection received from {} to {} at {:?}", connection.peer_socket_addr, connection.local_socket_addr, connection.date_open);
+                    tokio::spawn(echo(connection));
+                }
+                Err(e) => {
+                    println!("Connection Error: {}", e);
+                }
+            }
+        }
+    }
     pub async fn stop() {}
+    pub async fn handle_connection() {}
 }
 
-struct TcpClient {
-}
+// TODO: add a connection pool to avoid DOS
+struct TcpClient {}
 
 impl TcpClient {
     pub async fn connect() {}
 }
-*/
 
+pub struct UdpServer {}
+
+pub struct UdpClient {}
+
+struct P2PClient {}
+
+enum Connection {
+    TCP(TcpConnection),
+    UDP(UdpConnection),
+}
+
+
+// TODO: SystemTime new format ?
 pub struct TcpConnection {
     pub stream: TcpStream,
-    pub local_SocketAddr: SocketAddr,
-    pub peer_SocketAddr: SocketAddr,
+    pub local_socket_addr: SocketAddr,
+    pub peer_socket_addr: SocketAddr,
     pub date_open: SystemTime,
     pub date_close: Option<SystemTime>,
     pub tcp_stream: Vec<(Request, Response)>,
@@ -57,15 +118,15 @@ pub struct TcpConnection {
 
 impl TcpConnection {
     pub fn new(stream: TcpStream) -> TcpConnection {
-        let local_SocketAddr = _get_local_SocketAddr(&stream);
-        let peer_SocketAddr = _get_peer_SocketAddr(&stream);
+        let local_socket_addr = _get_local_SocketAddr(&stream);
+        let peer_socket_addr = _get_peer_SocketAddr(&stream);
         let date_open = SystemTime::now();
         let date_close = None;
         let tcp_stream = Vec::new();
         TcpConnection {
             stream,
-            local_SocketAddr,
-            peer_SocketAddr,
+            local_socket_addr,
+            peer_socket_addr,
             date_open,
             date_close,
             tcp_stream,
@@ -74,17 +135,24 @@ impl TcpConnection {
 
     // TODO: Should return a slice here
     pub fn local_as_str(&self) -> String {
-        format!("{}:{}", self.local_SocketAddr.ip(), self.local_SocketAddr.port())
+        format!("{}:{}", self.local_socket_addr.ip(), self.local_socket_addr.port())
     }
 
     // TODO: Should return a slice here
     pub fn peer_as_str(&self) -> String {
-        format!("{}:{}", self.peer_SocketAddr.ip(), self.peer_SocketAddr.port())
+        format!("{}:{}", self.peer_socket_addr.ip(), self.peer_socket_addr.port())
     }
 
+    // TODO: should be in the server or client
+    pub fn send(&mut self) {}
+
+    // TODO: should be in the server or client
+    pub fn recv(&mut self) {}
 }
 
+pub struct UdpConnection {}
 
+// TODO: add src and dst
 pub struct Request {
     pub date: SystemTime,
     pub content: Vec<u8>,
@@ -104,6 +172,8 @@ impl Request {
     }
 }
 
+
+// TODO: add a src dst
 pub struct Response {
     pub date: SystemTime,
     pub content: Vec<u8>,
@@ -119,4 +189,15 @@ impl Response {
     pub fn size(&self) -> usize {
         size_of_val(&self.content[..])
     }
+}
+
+/// Handle the connection of the echo client
+async fn echo(stream: TcpConnection) -> io::Result<()> {
+    let stream = stream.stream;
+    let peer_addr = stream.peer_addr()?;
+    println!("Connection from {}:{}", peer_addr.ip(), peer_addr.port());
+    let (mut recv, mut send) = io::split(stream);
+    io::copy(&mut recv, &mut send).await?;
+    println!("Disconnection from {}:{}", peer_addr.ip(), peer_addr.port());
+    Ok(())
 }
